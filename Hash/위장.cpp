75aef05980e9b4f8e33e3f0073db39d63595fdd7
Murky <heemang2001>
2021-06-14 14:09:32 +0900

    위장

--- Hash/위장.cpp
+++ Hash/위장.cpp
@@ -21,10 +21,13 @@ int solution(vector<vector<string>> clothes)
 		answer *= iter->second + 1; // +1은 선택을 안하는 경우
 	}
 
+	//test
 	//for (auto elem : hashClothes)
 	//{
 	//	cout << "key : " << elem.first << " value : " << elem.second << endl;
 	//}
+
+	
 
 	return answer - 1; // -1은 아무것도 선택하지않는 경우를 뺀것
 }
